a0e7307ef92a8501ab383a5515a2a1c5ce3e1b82
DaniBlaney <daniblaney@gmail.com>
2019-05-18 08:03:50 -0400

    created 5 friends

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Buddy",
+		photo: "https://cdn.newsapi.com.au/image/v1/67a523605bca40778c6faaad93883a3b",
+		scores: [5, 1, 2, 5, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Mr. Fuzzy Pants",
+		photo: "https://www.aspca.org/sites/default/files/pet-planning_pet-trust-primer_main-image.jpg",
+		scores: [2, 5, 4, 1, 4, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Champ",
+		photo: "https://ewscripps.brightspotcdn.com/dims4/default/33973be/2147483647/strip/true/crop/640x480+0+0/resize/640x480!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F6f%2Fe3%2F93dea70443d49c9fd8625dc8f00e%2Ffrosty.jpeg",
+		scores: [1, 1, 3, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Stella",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYmSyQNP5Mx0ldXD-FKs-IUsbx6iF_npFWnbUluxNpQ6FymazzOw",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Cookie",
+		photo: "https://g.petango.com/photos/1557/e4b517c0-e69d-46e7-84f2-8ee868daaabd.jpg",
+		scores: [2, 4, 4, 4, 2, 4, 2, 1, 1, 3]
+	}
+];
+
+module.exports = friends;
